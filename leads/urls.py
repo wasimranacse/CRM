fc365d71279aa6a5f5ff56cd7c321e130ba861b4
RANA <wrana697@gmail.com>
2021-01-22 17:57:09 +0600

    Login view created

---
+++ leads/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import (
+    LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
+    # lead_list, lead_detail, lead_create, lead_update, lead_delete, 
+)
+
+app_name = "leads"
+
+urlpatterns = [
+    path('', LeadListView.as_view(), name='lead-list'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
+    path('create/', LeadCreateView.as_view(), name='lead-create')
+
+]
